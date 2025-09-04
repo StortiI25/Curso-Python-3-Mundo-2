216fb88ad93859f0b36d97de48ac04133632c4f5
StortiI25 <carlos.hs.storti@gmail.com>
2025-09-04 17:39:11 -0300

    For laços

--- Desafios/Laços de Repetição (for)/Dsf-021.py
+++ Desafios/Laços de Repetição (for)/Dsf-021.py
@@ -3,3 +3,30 @@
 #▸ A média da idade do grupo.
 #▸ Qual é o nome do homem mais velho.
 #▸ Quantas mulheres têm menos de 20 anos.
+
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher = 0
+
+for p in range(1, 5):
+    print(f'------------{p}º PESSOA ------------')
+    nome = str(input("Nome: ")).strip()
+    idade = int(input("Idade: "))
+    sexo = str(input("Sexo [M/F]: ")).strip()
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mn' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher += 1
+
+
+mediaidade = somaidade / 4 
+print (f'A media de idade do grupo é de {mediaidade} anos')
+print (f'O homem mais velho tem {maioridadehomem} anos e se chama {nomevelho}')
+print (f'O total de mulher com menos de 20 anos é {totmulher}')
